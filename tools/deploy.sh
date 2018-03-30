b03f65eef8ac9645bb84a060d2062cd7eb4cba69
shino <harimanmon@gmail.com>
2018-03-30 13:56:22 +0900

    feat(nginx): deployスクリプト修正

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -3,18 +3,22 @@ set -ex
 USERNAME=$USER
 BRANCH=$1
 
-FROM_MYSQL_CNF=/home/isucon/isubata/files/db/mysql/mysqld.cnf
+FROM_MYSQL_CNF=/home/isucon/isubata/files/db/mysql/mysql.conf.d/mysqld.cnf
 FROM_NGINX_CNF=/home/isucon/isubata/files/app/nginx.conf
 TO_MYSQL_CNF=/etc/mysql/mysql.conf.d/mysqld.cnf
-TO_NGNX_CNF=/etc/nginx/nginx.conf
+TO_NGINX_CNF=/etc/nginx/nginx.conf
+
 cd /home/isucon/isubata \
 && echo "$USERNAME deploying..." \
 && echo 'git' \
 && git pull origin $BRANCH \
-cp 
-&& echo 'push config files' \
+&& echo 'push mysql config file' \
+&& sudo cp $FROM_MYSQL_CNF $TO_MYSQL_CNF \
+&& echo 'push nginx config file' \
+&& sudo cp $FROM_NGINX_CNF $TO_NGINX_CNF \
+&& echo 'restart mysql' \
 && sudo service mysql restart \
-&& echo 'redis' \
+&& echo 'restart redis' \
 && sudo service redis-server restart \
 && redis-cli KEYS "*" | xargs redis-cli DEL \
 && echo 'restart service' \
