29a4d95d073a0d6090bb9978f42943276f223709
shino <harimanmon@gmail.com>
2018-03-30 12:59:47 +0900

    chore: mysql conf backup & schemaspy

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -ex
-IPADDR=$1
 USERNAME=$USER
+BRANCH=$1
+
+FROM_MYSQL_CNF=/home/isucon/isubata/files/db/mysql/mysqld.cnf
+FROM_NGINX_CNF=/home/isucon/isubata/files/app/nginx.conf
+TO_MYSQL_CNF=/etc/mysql/mysql.conf.d/mysqld.cnf
+TO_NGNX_CNF=/etc/nginx/nginx.conf
 cd /home/isucon/isubata \
 && echo "$USERNAME deploying..." \
 && echo 'git' \
-&& git pull origin master \
+&& git pull origin $BRANCH \
+cp 
 && echo 'push config files' \
 && sudo service mysql restart \
 && echo 'redis' \
@@ -13,5 +19,6 @@ cd /home/isucon/isubata \
 && redis-cli KEYS "*" | xargs redis-cli DEL \
 && echo 'restart service' \
 && sudo systemctl restart isubata.nodejs.service \
+# && sudo rm /var/log/nginx/access.log \ 
 && sudo service nginx restart \
 && echo "$USERNAME deploy done" \
